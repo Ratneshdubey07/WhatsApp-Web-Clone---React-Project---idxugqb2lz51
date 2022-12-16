9a2a0bd9f1a7bfe5a23e65bd912c9d432701071e
Ratneshdubey07 <111305768+Ratneshdubey07@users.noreply.github.com>
2022-12-16 14:40:17 +0530

    Update Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -133,6 +133,9 @@ export default function Chat({ userName }) {
                     <button style={{ border: "none" }}>
                         <span className="material-symbols-outlined">mic</span>
                     </button>
+                     <button style={{ border: "none" }}>
+                        <span className="material-symbols-outlined">mic</span>
+                    </button>
                 </form>
             </div>
         </div>
